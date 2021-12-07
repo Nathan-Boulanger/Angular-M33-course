4953b67fa546c6acfff7c57403c3c672ae61dbe1
Nathan <nathanb@theodo.fr>
2021-12-07 17:04:14 +0100

    ngFor ngIf ...

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,16 +4,22 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   selector: 'event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{ event.name }}</h2>
-      <div>Date: {{ event.date }}</div>
-      <div>Time: {{ event.time }}</div>
-      <div>Price: \${{ event.price }}</div>
-      <div>
-        <span>Location: {{ event.location.address }}</span>
+      <h2>{{ event?.name }}</h2>
+      <div>Date: {{ event?.date }}</div>
+      <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+        Time: {{ event?.time }}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div>Price: \${{ event?.price }}</div>
+      <div [hidden]="!event?.location">
+        <span>Location: {{ event?.location?.address }}</span>
         <span class="pad-left">
-          {{ event.location.city }}, {{ event.location.country }}</span
+          {{ event?.location?.city }}, {{ event?.location?.country }}</span
         >
       </div>
+      <div *ngIf="event?.onlineUrl">Online URL: {{ event?.onlineUrl }}</div>
     </div>
   `,
   styles: [
@@ -27,9 +33,22 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
       .thumbnail {
         min-height: 210px;
       }
+
+      .green {
+        color: #003300 !important;
+      }
+
+      .bold {
+        font-weight: bold;
+      }
     `,
   ],
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+
+  getStartTimeClass() {
+    if (this.event && this.event.time === '8:00 am') return ['green', 'bold'];
+    return [];
+  }
 }
