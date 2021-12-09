4381aca5c24cbc0a3ad3d98d5e8af074a08c0477
Nathan <nathanb@theodo.fr>
2021-12-09 13:55:49 +0100

    Routing OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { ToastrService } from './common/toastr.service';
+import { Error404Component } from './errors/404.component';
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventService,
+  EventListResolver,
+  EventRouteActivator,
+} from './events';
 import { EventsAppComponent } from './events-app.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { EventsListComponent } from './events/events-list.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { appRoutes } from './routes';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
     NavBarComponent,
+    Error404Component,
+  ],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    EventListResolver,
+    { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState },
   ],
   bootstrap: [EventsAppComponent],
 })
 export class AppModule {}
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty) return window.confirm('Sure ?');
+  return true;
+}
